b51b37fffd4f64ac24504b7e781cf2547aabc34e
Feekohur <dudadudadudadudada@gmail.com>
2023-01-19 22:21:52 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfZadanie1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Maszyna maszyna;
+        public MainWindow()
+        {
+            InitializeComponent();
+            maszyna = new Maszyna();
+        }
+
+        private void Wrzuc_Click(object sender, RoutedEventArgs e)
+        {
+            if (Tekst.Text == "")
+            {
+                MessageBox.Show("Nie można dodać pustego napisu!");
+                Wynik.Content = "Wystąpił błąd!\n";
+                Wynik.Content += "Metoda: Wrzuc()\n";
+                Wynik.Content += maszyna.Zawartosc();
+            }
+            else
+            {
+                maszyna.Dodaj(Tekst.Text);
+                Wynik.Content = "Dodano napis: " + Tekst.Text +"\n";
+                Wynik.Content += "Metoda: Wrzuc()\n";
+                Wynik.Content += maszyna.Zawartosc();
+            }
+            
+        }
+
+        private void Wyjmij_Click(object sender, RoutedEventArgs e)
+        {
+            if (maszyna.CzySaKupony())
+            {
+                string element = maszyna.Losuj();
+                Wynik.Content = "Wylosowany napis: " + element + "\n";
+                Wynik.Content += "Metoda: Wyjmij()\n";
+                Wynik.Content += maszyna.Zawartosc();
+            }
+            else
+            {
+                MessageBox.Show("Nie ma elementów w maszynie!");
+                Wynik.Content = "Wystąpił błąd!\n";
+                Wynik.Content += "Metoda: Wyjmij()\n";
+                Wynik.Content += maszyna.Zawartosc();
+            }
+            
+        }
+
+    }
+}
---
+++ Maszyna.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfZadanie1
+{
+    class Maszyna
+    {
+        List<string> kupony;
+        Random random;
+
+        public Maszyna()
+        {
+            kupony = new List<string>();
+        }
+
+        public void Dodaj(string kupon)
+        {
+            kupony.Add(kupon);
+        }
+        public string Losuj()
+        {
+            random = new Random();
+            int i = random.Next(kupony.Count);
+            string kupon = kupony[i];
+            kupony.RemoveAt(i);
+            return kupon;
+        }
+
+        public bool CzySaKupony()
+        {
+            if (kupony.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Zawartosc()
+        {
+            string zawartosc = "Zawartość: ";
+            foreach (string kupon in kupony){
+                zawartosc += kupon + " ";
+            }
+            return zawartosc;
+        }
+
+    }
+}
